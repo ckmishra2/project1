d8b58286920cb7ce933361854ca7fc37545d6ee8
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-08-11 18:11:49 +0530

    Calculate Wages till a condition of total working hours or days is reached for a month

--- Employeewage.sh
+++ Employeewage.sh
@@ -1,12 +1,16 @@
+#!bin/bash -x
 echo "Welcome To Employee Wage Computation Program"
-check=$((RANDOM%3))
-echo $check
-wph=20
+wph=100
 fdh=8
 pth=4
 salary=0
+total=0
+for((i=1;i<=20;i++))
+do
 
-case $check in
+    check=$((RANDOM%3))
+    echo $check
+    case $check in
 		0)
 			echo "He was absent"
 			salary=0
@@ -25,4 +29,6 @@ case $check in
 			echo $salary
 		;;
 esac
-
+total=$((total+salary))
+done
+echo  "the monthly salary he earn is $total"
