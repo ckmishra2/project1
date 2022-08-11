be531cc4b6d8fbcbe3e1979c15063c1795f495a1
ckmishra2 <82533641+ckmishra2@users.noreply.github.com>
2022-08-11 17:51:26 +0530

    Solving using Switch case statement (#5)

--- Employeewage.sh
+++ Employeewage.sh
@@ -4,3 +4,25 @@ echo $check
 wph=20
 fdh=8
 pth=4
+salary=0
+
+case $check in
+		0)
+			echo "He was absent"
+			salary=0
+			echo $salary
+		;;
+
+		1)
+			echo "He was present on that day"
+			salary=$((fdh*wph))
+			echo $salary
+		;;
+
+		2)
+			echo "He took half a day"
+			salary=$((pth*wph))
+			echo $salary
+		;;
+esac
+
