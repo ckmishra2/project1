846e7ca561430dedddc87caeec850fffdd725bd6
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-08-12 13:14:31 +0530

    Dictionary to array

--- expression.sh
+++ expression.sh
@@ -1,4 +1,5 @@
 #! bin/bash -x
+declare -a result=()
 declare -A dict
 echo "Enter 1st input"
 read a
@@ -6,9 +7,14 @@ echo "Enter 2nd input"
 read b
 echo "Enter 3rd input"
 read c
+
+result+=($(($a+$b*$c)))
+result+=($(($a*$b+$c)))
+result+=($(($c+$a/$c)))
+
 dict[A]=$((a+b*c))
 dict[B]=$((a*b+c))
 dict[C]=$((c+a/c))
 echo ${dict[@]}
-#echo ${result[@]}
+echo ${result[@]}
 
